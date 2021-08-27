52cb1894d3e4ffaf9fb1034e98b82055c37f5cfd
William Francis <williamfrancis006@gmail.com>
2021-08-27 10:05:54 -0500

    Removed .py Solution to JS Challenge

--- typeSolver.py
+++
@@ -1,4 +0,0 @@
-import keyboard, time
-
-time.sleep(3)
-keyboard.write("In order to complete this challenge you must be able to type at super human speeds. Unless you are a robot or some kind of program I doubt that you will be able to complete it.")
